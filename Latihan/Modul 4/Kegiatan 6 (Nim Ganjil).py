9c021a6b1a4ce5434b5c9df0515909bff0b38779
Riski Cahyadi <96654537+Jnckk@users.noreply.github.com>
2023-11-15 17:19:08 +0700

    Delete Latihan/Modul 4 directory

--- Latihan/Modul 4/Kegiatan 6 (Nim Ganjil).py
+++
@@ -1,9 +0,0 @@
-def point(x, y):
-    return x, y
-
-def line_equation_of(x1, y1, m):
-    c = y1 - m * x1  # Menghitung nilai C
-    return f"y = {m:.2f}x + {c:.2f}"
-
-print("Persamaan garis yang melalui titik (2,-7) dan bergradien -9:") #202110370311279
-print(line_equation_of(2, -7, -9))
